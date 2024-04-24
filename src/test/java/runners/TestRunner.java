277b1732e656bb84c38325a73b9f2c400198fd3f
Mikiyas Kidane <mikiyaskidane17@gmail.com>
2024-04-24 16:31:47 -0400

    First Day prepare framework

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,9 @@
+package runners;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunner {
+    @Karate.Test
+    Karate runTest(){
+    return Karate.run("classpath:features/SecurityTest.feature");}
+}
